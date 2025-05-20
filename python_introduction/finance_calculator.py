d12ad6774e5c6ce559236922f015211af8abd9ef
Vedaste MUGABO <vedasteapp@gmail.com>
2025-05-21 00:55:07 +0200

    finace calculator program

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-salary = int(input("Enter your monthly income:"))
-expense = int(input("Enter your total monthly expenses:"))
-msaving = salary - expense
-asaving = msaving * 12 + (msaving * 12 * 0.05)
-print("Your monthly savings are $",msaving)
-print("Projected savings after one year, with interest, is: $",asaving)
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_savings = monthly_income - monthly_expenses
+Projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print("Your monthly savings are $",monthly_savings)
+print("Projected savings after one year, with interest, is: $",Projected_savings)
