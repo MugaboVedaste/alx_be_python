77f3bcae27aa6dd7237e1a455f827d8b201ec25b
Vedaste MUGABO <vedasteapp@gmail.com>
2025-06-05 12:25:53 +0200

    Woring on Modules

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,10 +6,9 @@ def perform_operation(num1, num2, operation):
     elif(operation=="multiply"):
         return num1*num2
     elif(operation=="divide"):
-        if( num2 == 0):
-            print("Numerator must be different to 0")
-        else:
-            return num1/num2
+        if num2 == 0:
+            return "Numerator must be different to 0"
+        return num1/num2
     else:
         print("Invalid Choice")
 
